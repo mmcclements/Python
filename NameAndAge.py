c295ea5c1c708911eb194e4210e3e90b6475017e
mmcclements <114061914+mmcclements@users.noreply.github.com>
2022-12-14 09:54:29 +0000

    Add files via upload

---
+++ NameAndAge.py
@@ -0,0 +1,15 @@
+#string strForename = "";
+Forename = "Ryan"
+Surname = "Major"
+
+Age = 72
+
+Fullname = Forename + " " + Surname
+
+print(Forename)
+print(Surname)
+print("\nYour full name is " + Fullname)
+print("You are " + str(Age) + " years old.")
+
+Age += 1
+print("\nNext year you wil be " + str(Age))
